95ceea1eb101149b12c995d722f29149602771df
gianghvse05576 <gianghvse05576@gmail.com>
2025-10-19 18:51:20 +0700

    fix lint

--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -8,11 +8,6 @@ public static class GameEvents
     public static event Action<int> OnLevelUp;
     public static event Action OnGameOver;
 
-    public static event Action<GameObject> OnTetrominoSpawned;
-    public static event Action<GameObject> OnNextTetrominoCreated;
-
-    public static event Action<bool> OnPauseToggled;
-
     public static void RaiseScoreAdded(int amount) => OnScoreAdded?.Invoke(amount);
     public static void RaiseLineCleared(int lines) => OnLineCleared?.Invoke(lines);
     public static void RaiseLevelUp(int level) => OnLevelUp?.Invoke(level);
